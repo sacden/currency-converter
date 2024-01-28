e3ebd253e4d5d3d8ae0061bfaff12f19570bc29c
sacden <sacden2009@gmail.com>
2024-01-28 14:27:37 +0100

    feat: add design fixes

--- src/App.jsx
+++ src/App.jsx
@@ -14,41 +14,55 @@ function App() {
   return (
     <>
       <div className="card">
-        <select
-          className="btn"
-          onChange={(e) => {
-            let selectedCurrency = e.target.value;
-            const selectedRate = rates.find((rate) => rate[0] === selectedCurrency)[1];
-            setFirstCurrency([selectedCurrency, selectedRate]);
-          }}
-        >
-          {rates.map((rate) => (
-            <option key={rate[0]} value={rate[0]}>
-              {rate[0]}
-            </option>
-          ))}
-        </select>
-        <select
-          className="btn"
-          onChange={(e) => {
-            let selectedCurrency = e.target.value;
-            const selectedRate = rates.find((rate) => rate[0] === selectedCurrency)[1];
-            setSecondCurrency([selectedCurrency, selectedRate]);
-          }}
-        >
-          {rates.map((rate) => (
-            <option key={rate[0]} value={rate[0]}>
-              {rate[0]}
-            </option>
-          ))}
-        </select>
-        <div className="amount">Amount</div>
-        <input className="search__input" name="myInput" value={value} onChange={(e) => setValue(e.target.value)} />
+        <div className="title">CURRENCY CONVERTER</div>
+        <div className="box">
+          <div className="label">Amount</div>
+          <input className="search__input" name="myInput" value={value} onChange={(e) => setValue(e.target.value)} />
+        </div>
 
-        <h1>
-          {value * firstCurrency[1] * secondCurrency[1]} {secondCurrency[0]}{" "}
-        </h1>
+        <div className="box">
+          <div className="label">From</div>
+          <select
+            className="select"
+            value={firstCurrency[0]}
+            onChange={(e) => {
+              let selectedCurrency = e.target.value;
+              const selectedRate = rates.find((rate) => rate[0] === selectedCurrency)[1];
+              setFirstCurrency([selectedCurrency, selectedRate]);
+            }}
+          >
+            {rates.map((rate) => (
+              <option key={rate[0]} value={rate[0]}>
+                {rate[0]}
+              </option>
+            ))}
+          </select>
+        </div>
 
+        <div className="box">
+          <div className="label">To</div>
+          <select
+            className="select"
+            value={secondCurrency[0]}
+            onChange={(e) => {
+              let selectedCurrency = e.target.value;
+              const selectedRate = rates.find((rate) => rate[0] === selectedCurrency)[1];
+              setSecondCurrency([selectedCurrency, selectedRate]);
+            }}
+          >
+            {rates.map((rate) => (
+              <option key={rate[0]} value={rate[0]}>
+                {rate[0]}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="result">
+          {(value * firstCurrency[1] * secondCurrency[1]).toFixed(2)} {secondCurrency[0]}{" "}
+        </div>
+
+        <div className="updated_time">{mockdata.time_last_update_utc}</div>
         {/* <h2>{firstCurrency}</h2>
         <h2>{secondCurrency}</h2> */}
       </div>
