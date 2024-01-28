c8cfbf44e916ac702e04f8a035e622855389acc9
sacden <sacden2009@gmail.com>
2024-01-28 11:43:28 +0100

    feat: add working funcionality with mockdata

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,51 @@
+import { useState } from "react";
 import "./App.css";
 import { mockdata } from "../mockdata";
 
 function App() {
-  if (!mockdata) {
+  const [rates, setRates] = useState(Object.entries(mockdata.rates));
+  const [firstCurrency, setFirstCurrency] = useState(["USD", 1]);
+  const [secondCurrency, setSecondCurrency] = useState(["CAD", 1.345104]);
+  const [value, setValue] = useState(1);
+
+  if (!rates) {
     <div>Loading...</div>;
   }
   return (
     <>
       <div className="card">
-        <select>
-          <option value="someOption">Some option</option>
-          <option value="otherOption">Other option</option>
-        </select>
-        <select>
-          <option value="someOption">Some option</option>
-          <option value="otherOption">Other option</option>
-        </select>
-        <input name="myInput" />
-        {/* <div>{JSON.stringify(mockdata)}</div> */}
-
-        <table>
-          {Object.entries(mockdata.rates).map((rate) => (
-            <tr>
-              <td>{rate[0]}</td>
-              <td>{rate[1]}</td>
-            </tr>
+        <select
+          onChange={(e) => {
+            let selectedCurrency = e.target.value;
+            const selectedRate = rates.find((rate) => rate[0] === selectedCurrency)[1];
+            setFirstCurrency([selectedCurrency, selectedRate]);
+          }}
+        >
+          {rates.map((rate) => (
+            <option key={rate[0]} value={rate[0]}>
+              {rate[0]}
+            </option>
           ))}
-        </table>
+        </select>
+        <select
+          onChange={(e) => {
+            let selectedCurrency = e.target.value;
+            const selectedRate = rates.find((rate) => rate[0] === selectedCurrency)[1];
+            setSecondCurrency([selectedCurrency, selectedRate]);
+          }}
+        >
+          {rates.map((rate) => (
+            <option key={rate[0]} value={rate[0]}>
+              {rate[0]}
+            </option>
+          ))}
+        </select>
+        <input name="myInput" value={value} onChange={(e) => setValue(e.target.value)} />
+
+        <h1>{value * firstCurrency[1] * secondCurrency[1]}</h1>
+
+        <h2>{firstCurrency}</h2>
+        <h2>{secondCurrency}</h2>
       </div>
     </>
   );
